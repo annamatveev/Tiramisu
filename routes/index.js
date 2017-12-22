c98711bda56cb9a2828972b610a782d7f0de3bba
annamat <anna.matveev@gmail.com>
2017-12-22 23:37:25 +0200

    Fixed page title Added bundle.js to .gitignore file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+var path = require('path');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
-    res.sendFile('public/index.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
