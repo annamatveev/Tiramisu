b5ac6b12ed8b150ebb0da1f4dfe18f78ce43e24e
annamat <anna.matveev@gmail.com>
2017-12-22 22:48:09 +0200

    Changed npm start to create bundle and run server Changed svg loader Moved static files to public dir Added express seed files

--- public/javascripts/components/App.js
+++ public/javascripts/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
-import '../styles/App.css';
+import Logo from '../../images/logo.svg';
+import '../../stylesheets/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Logo className="App-logo" alt="logo" />
+          <h1 className="App-title"> WasfasWelcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload!
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import '../stylesheets/index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
