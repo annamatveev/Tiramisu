d936799fa03201d3a63ae895eaed489d8b7e4ed9
annamat <anna.matveev@gmail.com>
2017-12-24 01:35:38 +0200

    Fixing CSS without loader

--- app/components/NavBar/NavBar.js
+++ app/components/NavBar/NavBar.js
@@ -18,41 +18,43 @@ export default class NavBar extends Component {
         const { activeItem } = this.state
 
         return (
-            <Sidebar as={Menu} animation='push' direction='top' visible={true} width='thin'>
-                <Menu.Item
-                    name='home'
-                    active={activeItem === 'home'}
-                    content='HOME'
-                    onClick={this.handleItemClick}
-                />
+            <nav id="menu-wrapper">
+                <Sidebar as={Menu} animation='push' direction='top' visible={true} width='thin'>
+                    <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        content='HOME'
+                        onClick={this.handleItemClick}
+                    />
 
-                <Menu.Item
-                    name='about'
-                    active={activeItem === 'about'}
-                    content='ABOUT'
-                    onClick={this.handleItemClick}
-                />
+                    <Menu.Item
+                        name='about'
+                        active={activeItem === 'about'}
+                        content='ABOUT'
+                        onClick={this.handleItemClick}
+                    />
 
-                <Menu.Item
-                    name='experience'
-                    active={activeItem === 'experience'}
-                    content='EXPERIENCE'
-                    onClick={this.handleItemClick}
-                />
+                    <Menu.Item
+                        name='experience'
+                        active={activeItem === 'experience'}
+                        content='EXPERIENCE'
+                        onClick={this.handleItemClick}
+                    />
 
-                <Menu.Item
-                    name='projects'
-                    active={activeItem === 'projects'}
-                    content='PROJECTS'
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='contact'
-                    active={activeItem === 'contact'}
-                    content='CONTACT'
-                    onClick={this.handleItemClick}
-                />
-            </Sidebar>
+                    <Menu.Item
+                        name='projects'
+                        active={activeItem === 'projects'}
+                        content='PROJECTS'
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='contact'
+                        active={activeItem === 'contact'}
+                        content='CONTACT'
+                        onClick={this.handleItemClick}
+                    />
+                </Sidebar>
+            </nav>
         );
     }
 }
