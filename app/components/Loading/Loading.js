0c9800c95c63be60f244cf63b10b72e9873b6962
annamat <anna.matveev@gmail.com>
2017-12-24 00:44:48 +0200

    Trying to dim loading area before unmounting

--- app/components/Loading/Loading.js
+++ app/components/Loading/Loading.js
@@ -4,11 +4,20 @@ import { Dimmer, Loader, Segment } from 'semantic-ui-react'
 import './Loading.css';
 
 class Loading extends Component {
+    state = {}
+    componentWillMount() {
+        this.setState({ active: true })
+    }
 
+    componentWillUnmount() {
+        this.setState({ active: false })
+    }
     render() {
+        const { active } = this.state
+
         return (
-            <Dimmer.Dimmable as={Segment} blurring dimmed={this.props.loading}>
-                <Dimmer active={this.props.loading}>
+            <Dimmer.Dimmable as={Segment} blurring dimmed={active}>
+                <Dimmer active={active}>
                     <Loader>Loading</Loader>
                 </Dimmer>
             </Dimmer.Dimmable>
