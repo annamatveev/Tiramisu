e5ca993c65b80ef600f0a381c94933603f1b48e6
annamat <anna.matveev@gmail.com>
2017-12-23 23:59:21 +0200

    Typo with loading attribute name

--- app/components/Loading/Loading.js
+++ app/components/Loading/Loading.js
@@ -7,8 +7,8 @@ class Loading extends Component {
 
     render() {
         return (
-            <Dimmer.Dimmable as={Segment} blurring dimmed={this.props.active}>
-                <Dimmer active={this.props.active}>
+            <Dimmer.Dimmable as={Segment} blurring dimmed={this.props.loading}>
+                <Dimmer active={this.props.loading}>
                     <Loader>Loading</Loader>
                 </Dimmer>
             </Dimmer.Dimmable>
