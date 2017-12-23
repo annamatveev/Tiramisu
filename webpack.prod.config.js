1f93fed6058b867033c673c0c0b443c30635a8e2
annamat <anna.matveev@gmail.com>
2017-12-23 23:15:35 +0200

    Gave up, using two static files

---
+++ webpack.prod.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const PROD = JSON.parse(process.env.PROD_ENV || '0');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+
+module.exports = {
+    entry: "./app/index.js",
+    output: {
+        filename: "./server/public/javascripts/bundle.js"
+    },
+    devtool: PROD? 'cheap-module-eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [require('autoprefixer')]
+                    }}]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    "babel-loader",
+                    {
+                        loader: "react-svg-loader",
+                        options: {
+                            svgo: {
+                                plugins: [
+                                    { removeTitle: false }
+                                ],
+                                floatPrecision: 2
+                            }
+                        }
+                    }
+                ]
+            }
+
+        ]
+    }
+    plugins:
+        [
+            new UglifyJsPlugin({
+                test: /bundle.js($|\?)/i,
+                cache: true
+            }),
+        ]
+
+}
