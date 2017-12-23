91b443fcd255c0694cbd8799bf74bbd757292450
annamat <anna.matveev@gmail.com>
2017-12-23 23:48:18 +0200

    Change to show loading by default

--- app/containers/CV/CV.js
+++ app/containers/CV/CV.js
@@ -22,7 +22,7 @@ class CV extends Component {
     }
 
     render() {
-        if (this.props === [] && this.props.loading) {
+        if (this.props.CV === [] && this.props.loading) {
             return <Loading loading={this.props.loading}/>;
         }
         const DetailsSection = Section(Details);
