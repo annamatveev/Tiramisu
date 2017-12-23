c66c8f34cb917648d729c84287db20812152bb07
annamat <anna.matveev@gmail.com>
2017-12-23 19:58:37 +0200

    Minimize even more

--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,30 @@ module.exports = {
     watch: true,
     plugins:
         [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            }),
             new UglifyJsPlugin({
                 test: /bundle.js($|\?)/i,
+                compress: {
+                    warnings: false,
+                    screw_ie8: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true
+                },
+                output: {
+                    comments: false
+                },
                 cache: true
             }),
+            new webpack.HashedModuleIdsPlugin(),
+
         ]
 
 }
