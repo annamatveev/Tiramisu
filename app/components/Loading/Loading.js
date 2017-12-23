2fc5c45088160b1f8b63e63398c9f556e179d648
annamat <anna.matveev@gmail.com>
2017-12-24 01:07:33 +0200

    Removed #menu-wrapper div

--- app/components/Loading/Loading.js
+++ app/components/Loading/Loading.js
@@ -4,7 +4,7 @@ import { Dimmer, Loader, Segment } from 'semantic-ui-react'
 import './Loading.css';
 
 class Loading extends Component {
-    state = {}
+    state = { }
     componentDidMount() {
         this.setState({ active: true })
     }
