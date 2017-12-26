5aeebe5ec45d7fe06a88fe0eb909149f8cfb5b4c
annamat <anna.matveev@gmail.com>
2017-12-26 11:16:50 +0200

    Adding URL const

--- app/containers/CV/saga.js
+++ app/containers/CV/saga.js
@@ -1,13 +1,15 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { fetchError, fetchSuccess } from '../../actions/CV'
 
+const CV_URL = '/api/getJsonCV'
+
 export function* fetchCVJSONDetails() {
     yield takeEvery('FETCH_REQUEST', loadDetails);
 }
 
 function* loadDetails() {
     try {
-        const response = yield call(fetch, '/api/getJsonCV');
+        const response = yield call(fetch, CV_URL);
         const details = yield response.json();
         yield put(fetchSuccess(details))
     }
