29ab141287e0839d6e52ccb143ac32464bdc6699
annamat <anna.matveev@gmail.com>
2017-12-26 11:29:45 +0200

    commiting file for heroku

--- server/middleware/frontEndMiddlewareSetup.js
+++ server/middleware/frontEndMiddlewareSetup.js
@@ -2,6 +2,7 @@ module.exports = (app, options) => {
     const isProd = process.env.NODE_ENV === 'production';
 
     if (isProd) {
+
         const prodMiddleware = require('./prodMiddleware');
         prodMiddleware(app, options);
     } else {
