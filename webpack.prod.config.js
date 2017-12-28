c84ed0de7889d63a5b24e713364367de3fb50fe3
annamat <anna.matveev@gmail.com>
2017-12-28 02:23:33 +0200

    Add NODE_ENV config to webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -31,6 +31,11 @@ module.exports = {
     },
     plugins:
         [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                },
+            }),
             new UglifyJsPlugin()
         ]
 
